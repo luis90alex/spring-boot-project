9e415eb3ac397e4b208e039529f78e60cf8d598e
luis90alex <luis90alex@gmail.com>
2025-08-23 14:12:09 +0200

    Starting Maven project and adding dependencies

---
+++ spring-boot-demo-store/src/main/java/com/learntocodewithluis/spring_boot_demo_store/SpringBootDemoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.learntocodewithluis.spring_boot_demo_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoStoreApplication.class, args);
+	}
+
+}
---
+++ spring-boot-demo-store/src/test/java/com/learntocodewithluis/spring_boot_demo_store/SpringBootDemoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntocodewithluis.spring_boot_demo_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
